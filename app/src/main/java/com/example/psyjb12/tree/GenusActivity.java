c0aae528d71eecf65253dc74b8bd43f130c14134
JPUF <2009bennettj@gmail.com>
2019-01-29 16:31:25 +0000

    Demonstrates basic navigation between activities.

---
+++ app/src/main/java/com/example/psyjb12/tree/GenusActivity.java
@@ -0,0 +1,13 @@
+package com.example.psyjb12.tree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genus);
+    }
+}
--- app/src/main/java/com/example/psyjb12/tree/SpeciesActivity.java
+++ app/src/main/java/com/example/psyjb12/tree/SpeciesActivity.java
@@ -1,24 +1,28 @@
 package com.example.psyjb12.tree;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SpeciesActivity extends AppCompatActivity {
 
     TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_species);
          tv = (TextView) findViewById(R.id.scientific_name);
          tv.setText("Larix decidua");
 
          findViewById(R.id.dist0).bringToFront();
          findViewById(R.id.dist1).bringToFront();
+    }
+
+    public void toGenus(View view) {
+        Intent intent = new Intent(this, GenusActivity.class);
+        startActivity(intent);
     }
 }
