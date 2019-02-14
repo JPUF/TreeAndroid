8a298cb629a896da1986a9a3fadfb2bd3d64efb8
psyjb12 <psyjb12@nottingham.ac.uk>
2019-02-14 14:06:44 +0000

    A (currently) volatile implementation of vernacular name search.

--- app/src/main/java/com/example/psyjb12/tree/HomeActivity.java
+++ app/src/main/java/com/example/psyjb12/tree/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.psyjb12.tree;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,9 +37,56 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void toSpecies(View view) {
+    public void goToSpeciesActivity(String scientificName) {
         Intent intent = new Intent(this, SpeciesActivity.class);
+        intent.putExtra(TREE_SEARCH, scientificName);
         startActivity(intent);
     }
 
+    public void getScientificFromVernacular(View view) {
+        EditText editText = (EditText) findViewById(R.id.vernacularText);
+        final String vernacularText = editText.getText().toString();
+        Log.i("vernacular", vernacularText);
+
+        new Thread() {
+            public void run() {
+                try {
+                    String urlString = "http://api.gbif.org/v1/species/search?q="+vernacularText;
+                    URL url = new URL(urlString);
+                    URLConnection request = url.openConnection();
+                    request.connect();
+
+                    JsonParser jp = new JsonParser();
+                    JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+                    JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+                    JsonArray results= rootobj.get("results").getAsJsonArray();
+                    String scientificName;
+                    JsonElement result = results.get(0);
+                    try{
+                        scientificName = result.getAsJsonObject().get("canonicalName").toString();
+                    } catch (NullPointerException e) {
+                        scientificName = result.getAsJsonObject().get("scientificName").toString();
+                    }
+                    scientificName = scientificName.substring(1,scientificName.length()-1);
+                    Log.i("vernacular", scientificName);
+
+                    goToSpeciesActivity(scientificName);
+
+                    /*
+                    for (JsonElement result : results) {//To iterate through all search results. (needed in case the desired species isn't result #1)
+                        try{
+                            scientificName = result.getAsJsonObject().get("canonicalName").toString();
+                        } catch (NullPointerException e) {
+                            scientificName = result.getAsJsonObject().get("scientificName").toString();
+                        }
+                        Log.i("vernacular", scientificName);
+                    }
+                    */
+
+                }catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }.start();
+    }
 }
