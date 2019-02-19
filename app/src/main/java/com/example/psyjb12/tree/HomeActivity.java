9038549485a0884413106594120db327d922e07c
JPUF <2009bennettj@gmail.com>
2019-02-19 13:02:59 +0000

    Search results now display numerous relevant vernacular names for each result. This is still very volatile.

--- app/src/main/java/com/example/psyjb12/tree/HomeActivity.java
+++ app/src/main/java/com/example/psyjb12/tree/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.psyjb12.tree;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -77,6 +78,14 @@ public class HomeActivity extends AppCompatActivity {
         new Thread() {
             public void run() {
                 try {
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            search_results_ll.removeAllViews();//remove any previous search results.
+                        }
+                    });
+
                     String urlString = "http://api.gbif.org/v1/species/search?q=" + URLEncoder.encode(searchTerm,"UTF-8");
                     URL url = new URL(urlString);
                     URLConnection request = url.openConnection();
@@ -90,14 +99,17 @@ public class HomeActivity extends AppCompatActivity {
 
                     //TODO handle genus results (e.g. search for 'Birch'/'Betula'. Should be able to go straight to Betula genus page.)
 
-                    ArrayList<String> uniqueNames = new ArrayList<>();
+                    ArrayList<String> uniqueNames = new ArrayList<>();//a list of valid and unique scientific names.
                     for (JsonElement result : results) {//To iterate through all search results. (needed in case the desired species isn't result #1)
                         try{
                             scientificName = result.getAsJsonObject().get("canonicalName").toString();
                             scientificName = scientificName.substring(1,scientificName.length()-1);
                             if(!uniqueNames.contains(scientificName)) {//if current name is unique.
-                                uniqueNames.add(scientificName);
-                                Log.i("scientificName", "canon: "+scientificName);
+                                if(result.getAsJsonObject().get("kingdom").toString().contains("Plantae")) { //if name belongs to a plant.
+                                    uniqueNames.add(scientificName);
+                                    Log.i("scientificName", "canon: " + scientificName);
+                                    Log.i("scientificName", "kingdom: " + result.getAsJsonObject().get("kingdom").toString());
+                                }
                             }
 
                         } catch (NullPointerException e) {
@@ -105,17 +117,57 @@ public class HomeActivity extends AppCompatActivity {
                             scientificName = scientificName.substring(1,scientificName.length()-1);
                             if(!uniqueNames.contains(scientificName)) {//if current name is unique.
                                 uniqueNames.add(scientificName);
-                                Log.i("scientificName", " sciN: "+scientificName);
+                                Log.i("scientificName", " sciN: " + scientificName);
                             }
                         }
                     }
 
+
+                    final ArrayList<String> vernacularNames = new ArrayList<>();
                     for (String name : uniqueNames) {
+                        vernacularNames.clear();
+                        Log.i("vernacular", "result");
                         final String finalName = name;
+                        urlString = "http://api.gbif.org/v1/species/search?q='"+ URLEncoder.encode(finalName, "UTF-8")+"'&limit=1";
+                        url = new URL(urlString);
+                        request = url.openConnection();
+                        request.connect();
+                        //jp = new JsonParser();
+                        root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+                        JsonObject rootobj = root.getAsJsonObject();
+                        JsonArray nameResults = rootobj.get("results").getAsJsonArray();
+                        JsonElement topResult  = nameResults.getAsJsonArray().get(0);
+                        JsonElement nameElement = topResult.getAsJsonObject().get("nubKey");
+                        final String GBIF_id = nameElement.getAsString();
+
+                        urlString = "http://api.gbif.org/v1/species/"+URLEncoder.encode(GBIF_id, "UTF-8")+"/vernacularNames";
+                        url = new URL(urlString);
+                        request = url.openConnection();
+                        request.connect();
+
+                        //jp = new JsonParser();
+                        root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+                        rootObject = root.getAsJsonObject(); //May be an array, may be an object.
+                        results= rootObject.get("results").getAsJsonArray();
+                        String vernacularName;
+                        for (JsonElement result : results) {
+                            try {
+                                if(result.getAsJsonObject().get("language").toString().contains("eng")) {//if vernacular name is in English.
+                                    vernacularName = result.getAsJsonObject().get("vernacularName").toString();
+                                    vernacularName = vernacularName.substring(1,vernacularName.length()-1);
+                                    if(!vernacularNames.contains(vernacularName)){//if name hasn't been added before.
+                                        vernacularNames.add(vernacularName);
+                                        Log.i("vernacular", "       vernacular: " + vernacularName);
+                                    }
+                                }
+                            } catch (NullPointerException e) {
+                                e.printStackTrace();
+                            }
+                        }
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                search_results_ll.addView(createTreeCard(finalName));
+                                search_results_ll.addView(createTreeCard(finalName, vernacularNames));
                             }
                         });
                     }
@@ -174,32 +226,53 @@ public class HomeActivity extends AppCompatActivity {
         }.start();
     }
 
-    public CardView createTreeCard(String name) {
-        Context context = getApplicationContext();
+    public CardView createTreeCard(String name, ArrayList<String> vernacularNames) {
+        StringBuilder sb = new StringBuilder();
+        for (String n : vernacularNames) {
+            sb.append(n);
+            sb.append(", ");
+        }
+        String allNames = sb.toString();
+        allNames = allNames.substring(0, allNames.length()-2);
 
+        Context context = getApplicationContext();
         CardView cv = new CardView(context);
         ViewGroup.LayoutParams cvParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         cv.setLayoutParams(cvParams);
         cv.setPadding(0,2,0,0);
 
-        LinearLayout ll = new LinearLayout(context);
+        LinearLayout outer_layout = new LinearLayout(context);
         LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        ll.setLayoutParams(llParams);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
+        outer_layout.setLayoutParams(llParams);
+        outer_layout.setOrientation(LinearLayout.HORIZONTAL);
 
         ImageView iv = new ImageView(context);
         iv.setImageDrawable(getBaseContext().getDrawable(R.drawable.yew_fruit));
         LinearLayout.LayoutParams ivParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         iv.setLayoutParams(ivParams);
 
-        TextView tv = new TextView(context);
-        LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        tv.setLayoutParams(tvParams);
-        tv.setText(name);
+        LinearLayout inner_layout = new LinearLayout(context);
+        inner_layout.setLayoutParams(llParams);
+        inner_layout.setOrientation(LinearLayout.VERTICAL);
 
-        ll.addView(iv);
-        ll.addView(tv);
-        cv.addView(ll);
+        TextView title_tv = new TextView(context);
+        LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        title_tv.setLayoutParams(tvParams);
+        title_tv.setPadding(4, 0, 0, 0);
+        title_tv.setTypeface(title_tv.getTypeface(), Typeface.BOLD);
+        title_tv.setText(name);
+
+        TextView vernacular_tv = new TextView(context);
+        vernacular_tv.setLayoutParams(tvParams);
+        vernacular_tv.setPadding(8, 0, 0 ,0);
+        vernacular_tv.setTextSize(12);
+        vernacular_tv.setText(allNames);
+
+        outer_layout.addView(iv);
+        inner_layout.addView(title_tv);
+        inner_layout.addView(vernacular_tv);
+        outer_layout.addView(inner_layout);
+        cv.addView(outer_layout);
 
         return cv;
     }
